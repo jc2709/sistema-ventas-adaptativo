37ba76e22d0b0254b6805456942530316a16c6f0
Juan Medina <bastianperrogordo@gmail.com>
2026-09-03 11:47:00 -0500

    Amplía catálogo de emociones del modelo

--- lib/processing/emotion/emotion_type.dart
+++ lib/processing/emotion/emotion_type.dart
@@ -1,8 +1,23 @@
 enum EmotionType {
   happy,
-  neutral,
   sad,
-  angry,
   surprise,
+  fear,
+  angry,
+  disgust,
+  neutral,
   unknown,
+}
+
+extension EmotionTypeLabel on EmotionType {
+  String get displayName => switch (this) {
+        EmotionType.happy => 'Feliz',
+        EmotionType.sad => 'Triste',
+        EmotionType.surprise => 'Sorprendido',
+        EmotionType.fear => 'Miedo',
+        EmotionType.angry => 'Enojado',
+        EmotionType.disgust => 'Disgusto',
+        EmotionType.neutral => 'Neutral',
+        EmotionType.unknown => 'Desconocido',
+      };
 }
