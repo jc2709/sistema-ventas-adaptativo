16004f193fc9c6be12caad04ee99f171cfaae9af
Juan Medina <bastianperrogordo@gmail.com>
2026-09-02 23:56:45 -0500

    Agrega modelo de oferta

---
+++ lib/domain/models/offer.dart
@@ -0,0 +1,11 @@
+class Offer {
+  const Offer({
+    required this.id,
+    required this.title,
+    required this.discountPercentage,
+  });
+
+  final int id;
+  final String title;
+  final double discountPercentage;
+}
