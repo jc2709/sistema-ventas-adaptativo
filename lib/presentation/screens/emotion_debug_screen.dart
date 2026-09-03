21d250f743a39263844d85ddea5cb2b62f7aec76
Juan Medina <bastianperrogordo@gmail.com>
2026-09-02 23:56:51 -0500

    Agrega pantalla de prueba de emociones

---
+++ lib/presentation/screens/emotion_debug_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EmotionDebugScreen extends StatelessWidget {
+  const EmotionDebugScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Prueba de emociones: pendiente de integrar modelo TFLite'),
+      ),
+    );
+  }
+}
